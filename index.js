e81ac4b3c9361cb6e6ab999c6b187b63bb454967
umairnisar5 <49435408+umairnisar5@users.noreply.github.com>
2020-11-06 19:29:49 +0500

    update index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+var cloudElement = document.getElementById("cloud-div");
 
 
+ var  animatedElem = cloudElement.animate([
+    { transform: "translate(0 , 0 )" },
+    { transform:  "translate(600px , 0)"  },
 
+
+], {
+    duration: 5000,
+    iterations: Infinity,
+    playbackRate: -4,
+})
